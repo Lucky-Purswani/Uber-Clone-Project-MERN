db6f904eb653bf7c19fadfa83b916eaaa8f1eb4f
Lucky <manishaspurswani@gmail.com>
2025-07-08 14:12:22 +0530

    Loader spinner error in vercel fixing..

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,16 +1,15 @@
 // components/Loader.jsx
 import React from 'react';
-import { TailSpin } from 'react-loader-spinner';
+import { ClipLoader } from 'react-spinners';
 
 export default function Loader() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <TailSpin
-        height={50}
-        width={50}
-        color="#000000" // Black loader to match Uber's theme
-        ariaLabel="loading"
-        visible={true}
+      <ClipLoader
+        color="#000000" // Uber-style black spinner
+        loading={true}
+        size={50}
+        aria-label="Loading Spinner"
       />
     </div>
   );
